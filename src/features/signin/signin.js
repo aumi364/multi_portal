c93e5f57de2518ebb2cf8b2d99e59c1f1987499a
Asif <aumi.asif@gmail.com>
2022-07-18 01:28:02 +0600

    theme upgraded

--- src/features/signin/signin.js
+++ src/features/signin/signin.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Button, Typography } from 'antd';
-import { Paragraph, Title } from '../../components/styled/Typography';
+import { Paragraph } from '../../components/styled/Typography';
 
 const Signin = props => {
   return <Paragraph>hello</Paragraph>;
