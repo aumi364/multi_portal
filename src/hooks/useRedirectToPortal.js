077f4887fd25b82ed59de7ff6075f6089441286a
Asif <aumi.asif@gmail.com>
2022-07-19 23:49:29 +0600

    redirect to portal removed

--- src/hooks/useRedirectToPortal.js
+++
@@ -1,6 +0,0 @@
-import { useGetCurrentUserRole } from './useGetCurrentUserRole';
-import { Navigate } from 'react-router';
-export const useRedirectToPortal = redirectPath => {
-  const currentUserRole = useGetCurrentUserRole();
-  return <Navigate to={`${currentUserRole}/${redirectPath}`} />;
-};
